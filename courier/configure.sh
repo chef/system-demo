1c9358c2e55533a441182468c19cdbc4ab29082b
GeorgeWestwater <george.westwater@progress.com>
2023-08-24 11:06:37 -0500

    Added logging

--- courier/configure.sh
+++ courier/configure.sh
@@ -1,30 +1,45 @@
-#cat file2 >> file1
+#!/bin/bash
 if [ "$EUID" -ne 0 ]
   then echo "script must run as root"
   exit
 fi
 
+echo "creating /opt/demo"
 mkdir -p /opt/demo
 cp -r cmd/* /opt/demo
 chown -R ec2-user:ec2-user /opt/demo
 chmod a+x /opt/demo/*
+
+echo "bin linking demo"
 ln -s /opt/demo/chef /usr/bin/
 ln -s /opt/demo/node /usr/bin/
 ln -s /opt/demo/courier /usr/bin/
 
-cp instructions.txt ../../
-cp commands.txt ../../
-cp -R content ../../content
-cp -R notes/ ../../notes
-cp -R demos/ ../../demos
+echo "copy instructions"
+cp instructions.txt /home/ec2-user/
+echo "copy commands"
+cp commands.txt /home/ec2-user/
+echo "copy content"
+cp -R content /home/ec2-user/content
+echo "copy notes"
+cp -R notes/ /home/ec2-user/notes
+echo "copy demos"
+cp -R demos/ /home/ec2-user/demos
 
+echo "fix owners"
 chmod -R a+r /home/ec2-user/content
 chmod -R a+r /home/ec2-user/notes
-chmod a+r /home/ec2-user/instructions.txt
-chmod a+r /home/ec2-user/commands.txt
 chown -R ec2-user:ec2-user /home/ec2-user/content
 chown -R ec2-user:ec2-user /home/ec2-user/notes
 chown -R ec2-user:ec2-user /home/ec2-user/instructions.txt
 chown -R ec2-user:ec2-user /home/ec2-user/commands.txt
-./init/nodes.sh
-./init/skills.sh
+
+echo "fix file permisions"
+chmod a+r /home/ec2-user/instructions.txt
+chmod a+r /home/ec2-user/commands.txt
+
+echo "init nodes"
+/home/ec2-user/system-demo/courier/init/nodes.sh
+
+echo "init skills"
+/home/ec2-user/system-demo/courier/init/skills.sh
--- courier/init/nodes.sh
+++ courier/init/nodes.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+echo "waiting for node API to be online"
 curl --retry 18 --retry-delay 10 -s -o /dev/null "http://192.168.0.10:9676/v1/nodes/"              
 function find() {
     curl -s http://192.168.0.10:9676/v1/nodes/ | jq -r --arg IP "ip-$1" '.data[] | select(.attributes[].value | contains($IP)) | .id'
 }
 
+echo "update .bashrc"
 echo "export NODE1=$(find "192-168-10-10")" >> /home/ec2-user/.bashrc
 echo "export NODE2=$(find "192-168-10-20")" >> /home/ec2-user/.bashrc
 echo "export NODE3=$(find "192-168-10-30")" >> /home/ec2-user/.bashrc
