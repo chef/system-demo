a150d5877d7cbbfa7e4b1494a44380090c81f832
GeorgeWestwater <george.westwater@progress.com>
2023-09-23 14:23:35 -0500

    testing first package

--- target-mode/demo/cookbooks/linux/recipes/default.rb
+++ target-mode/demo/cookbooks/linux/recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2023, The Authors, All Rights Reserved.
+
+switch_user 'sudo_su_root' do
+    user        'root'
+end
+
+yum 'yum-gi' do
+    package         'git'
+end
